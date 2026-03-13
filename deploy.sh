01f737d99ca09ea60aa02aa3203caf67742b0938
dncrwlye <dancrowley.e@gmail.com>
2026-03-13 14:10:57 -0600

    update site

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp ~/Users/dancrowley/Box Sync/cover_letters_and_resumes/epi_resume.pdf ./epi_resume.pdf
+cp "/Users/dancrowley/Box Sync/cover_letters_and_resumes/epi_resume.pdf" /Users/dancrowley/website/dncrwlye.github.io/epi_resume.pdf
 git add .
 git commit -m "update site"
 git push
